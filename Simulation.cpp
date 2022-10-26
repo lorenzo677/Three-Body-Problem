f0486034897059ee01816dd5e5727aff36128660
Lorenzo Barsotti <lorenzo.barsotti@hotmail.com>
2022-10-26 15:48:16 +0200

    Added function distance

--- Simulation.cpp
+++ Simulation.cpp
@@ -1,20 +1,25 @@
 #include <iostream>
+#include <cmath>
 
-void compute_vector_cm(float mass_1, float mass_2, float mass_3, float vector_1[3], float vector_2[3], float vector_3[3]) 
-{   
+#define DIM 3
+#define G 6.67408e-11
+#define N_BODIES 3
+
+void compute_vector_cm(float mass_1, float mass_2, float mass_3, float vector_1[3], float vector_2[3], float vector_3[3]){   
     float vector_cm[3];
     for (int i = 0; i < 4; i++) {   
         vector_cm[i] = ( mass_1 * vector_1[i] + mass_2 * vector_2[i] + mass_3 * vector_3[i] ) / (mass_1 + mass_2 + mass_3);
     }
 }
 
-int main()
-{
+float distance(float pos_1[DIM], float pos_2[DIM]){
+    return sqrt(pow(pos_1[0]-pos_2[0], 2) + pow(pos_1[1]-pos_2[1], 2) + pow(pos_1[2]-pos_2[2], 2));
+}
+
+int main(){
     // Definition of constants (be careful of Unit Measurements)
-    int n_bodies = 3;                   // number of bodies
-    float mass_1, mass_2, mass_3;       // masses
-    float x0_1, x0_2, x0_3;             // initial positions
-    const int G = 6.67408e-11;          // gravitational constant
+    float mass_1, mass_2, mass_3;                       // masses
+    float x0_1[DIM], x0_2[DIM], x0_3[DIM];              // initial positions
 
     return 0;
 }
