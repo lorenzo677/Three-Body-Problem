1b83ef901fdc675679417d152b4d9644d3123b38
Lorenzo Barsotti <lorenzo.barsotti@hotmail.com>
2022-10-27 09:34:04 +0200

    Modified the function for cm. now it returns an array

--- Simulation.cpp
+++ Simulation.cpp
@@ -1,16 +1,19 @@
 #include <iostream>
 #include <cmath>
+#include <array>
 #include "integrators.h"
 
 #define DIM 3
 #define G 6.67408e-11
 #define N_BODIES 3
+#define N_POINTS 1500
 
-void compute_vector_cm(float mass_1, float mass_2, float mass_3, float vector_1[3], float vector_2[3], float vector_3[3]){   
-    float vector_cm[3];
-    for (int i = 0; i < 4; i++) {   
+std::array<float, 3> compute_vector_cm(float mass_1, float mass_2, float mass_3, float *vector_1, float *vector_2, float *vector_3){   
+    std::array<float, 3> vector_cm;
+    for (int i = 0; i < 3; i++) {   
         vector_cm[i] = ( mass_1 * vector_1[i] + mass_2 * vector_2[i] + mass_3 * vector_3[i] ) / (mass_1 + mass_2 + mass_3);
     }
+    return vector_cm; 
 }
 
 float distance(float pos_1[DIM], float pos_2[DIM]){
@@ -19,11 +22,17 @@ float distance(float pos_1[DIM], float pos_2[DIM]){
 
 int main(){
     // Definition of constants (be careful of Unit Measurements)
-    float mass_1, mass_2, mass_3;                       // masses
-    float x0_1[DIM], x0_2[DIM], x0_3[DIM];              // initial positions
+    float mass_1 = 2e30, mass_2 = 3e30, mass_3 = 1e31;                       // masses
+    float x0_1[DIM], x0_2[DIM], x0_3[DIM];                                  // initial positions
 
+    //qua sotto per vedere se funziona compute_vector_cm
+    float a[3] = {3.0, 2.0, 3.4};
+    float b[3] = {2.0, 2.0, 40.4};
+    float c[3] = {2.0, 1.0, 4.4};
+    std::array<float, 3> cm;
+    cm = compute_vector_cm(4.0, 5.4, 6.2, a, b, c);
 
-
+    std::cout<<cm[0]<<'\t'<<cm[1]<<'\t'<<cm[2]<<std::endl;
 
     return 0;
 }
