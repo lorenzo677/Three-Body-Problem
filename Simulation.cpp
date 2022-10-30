ca1cd5937ae44fa530c67955a6e8809fe8fa8f4e
keivan-amini <keivan.amini@outlook.it>
2022-10-30 18:46:23 +0100

    added euler algorithm without the function

--- Simulation.cpp
+++ Simulation.cpp
@@ -20,10 +20,14 @@ float distance(float pos_1[DIM], float pos_2[DIM]){
     return sqrt(pow(pos_1[0]-pos_2[0], 2) + pow(pos_1[1]-pos_2[1], 2) + pow(pos_1[2]-pos_2[2], 2));
 }
 
-float acceleration(float mass_1, float mass_2, float pos_1, float pos_2, float pos_3){
+// Funzioni per le accelerazioni //
+
+float acceleration(float mass_1, float mass_2, float pos_1, float pos_2, float pos_3){ 
     //compute the acceleration along one axis of the body 3
     return G * (mass_1 / pow(pos_3-pos_1, 2) + mass_2 / pow(pos_3-pos_2, 2));
 }
+
+
 
 int main(){
     // Definition of constants (be careful of Unit Measurements)
@@ -32,9 +36,10 @@ int main(){
     float v0_1[DIM], v0_2[DIM], v0_3[DIM];                                  // initial velocity
     float h = 1;
 
-    float time[N_STEPS], x_1[DIM][N_STEPS], x_2[DIM][N_STEPS], x_3[DIM][N_STEPS], v_1[DIM][N_STEPS], v_2[DIM][N_STEPS], v_3[DIM][N_STEPS];
+    float time[N_STEPS], x_1[DIM][N_STEPS], x_2[DIM][N_STEPS], x_3[DIM][N_STEPS], v_1[DIM][N_STEPS], v_2[DIM][N_STEPS], v_3[DIM][N_STEPS], a_1[DIM][N_STEPS], a_2[DIM][N_STEPS], a_3[DIM][N_STEPS];
+          
 
-    for (int i=0;i<N_STEPS; i++){time[i]=1.0 * h;}
+    for (int i=0;i<N_STEPS; i++){time[i]= i * h;}
 
     x_1[0][0] = x0_1[0];
     x_2[0][0] = x0_2[0];
@@ -55,15 +60,38 @@ int main(){
     v_2[2][0] = v0_2[2];
     v_3[2][0] = v0_3[2];
 
-    for(int j=0; j<DIM; j++){
+/*
+   for(int j=0; j<DIM; j++){
         for(int i=0; i<N_STEPS; i++){
-           v_1[j][i+1] = RK4(time[i], v_1[j][i], h, gravity, mass_2, mass_3, x_2[j][i], x_3[j][i]); // probabilmente sbagliatto
+           v_1[j][i+1] = RK4(time[i], v_1[j][i], h, acceleration, mass_2, mass_3, x_2[j][i], x_3[j][i]); // probabilmente sbagliatto
         }
     }
-  
+*/
 
 
 
+
+//Function for the Euler method (contrasta con quella presente sull'header)
+for(int j=0; j<DIM; j++){
+    for (int i=0; i<N_STEPS; i++) {
+    
+    a_1[j][i] = acceleration(mass_2, mass_3, x_2[j][i], x_3[j][i], x_1[j][i]);
+    a_2[j][i] = acceleration(mass_1, mass_3, x_1[j][i], x_3[j][i], x_2[j][i]);
+    a_3[j][i] = acceleration(mass_1, mass_2, x_1[j][i], x_2[j][i], x_3[j][i]);
+
+    v_1[j][i + 1] = v_1[j][i] + a_1[j][i] * h;
+	v_2[j][i + 1] = v_2[j][i] + a_2[j][i] * h;
+	v_2[j][i + 1] = v_3[j][i] + a_3[j][i] * h;
+
+    x_1[j][i + 1] = x_1[j][i] + v_1[j][i] * h;
+    x_2[j][i + 1] = x_2[j][i] + v_2[j][i] * h;
+    x_3[j][i + 1] = x_3[j][i] + v_3[j][i] * h;
+
+}
+}
+
+    // diamo delle condizioni iniziali e stampiamo l'array x_1
+    // TODO
 
 
     //qua sotto per vedere se funziona compute_vector_cm
