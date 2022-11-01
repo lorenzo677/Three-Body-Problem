8c2860c03d82fea41b2b70a19bf8dbce87e87eff
keivan-amini <keivan.amini@outlook.it>
2022-11-01 15:14:11 +0100

    fixed some small bugs.. but not the big one!

--- Simulation.cpp
+++ Simulation.cpp
@@ -7,7 +7,7 @@
 #define DIM 4
 #define G  10//6.67408e-11
 #define N_BODIES 3
-#define N_STEPS 10000
+#define N_STEPS 1000
 
 std::array<float, 3> compute_vector_cm(float mass_1, float mass_2, float mass_3, float *vector_1, float *vector_2, float *vector_3){   
     std::array<float, 3> vector_cm;
@@ -31,8 +31,8 @@ float acceleration(float mass_1, float mass_2, float pos_1, float pos_2, float p
 int main(){
     // Definition of constants (be careful of Unit Measurements)
     double mass_1 = 10, mass_2 = 20, mass_3 = 30;                       // masses
-    double x0_1[DIM-1] = {-10, 10, -11}, x0_2[DIM-1] = {0, 0, 0}, x0_3[DIM-1] = {10, 10, 12};                                  // initial positions
-    double v0_1[DIM-1] = {-3, 0, 0}, v0_2[DIM-1] = {0, 0, 0}, v0_3[DIM-1] = {3, 0, 0};                                 // initial velocity
+    double x0_1[DIM-1] = {-10, 10, -11}, x0_2[DIM-1] = {0.5, 0.5, 0.5}, x0_3[DIM-1] = {10, 10, 12};                                  // initial positions
+    double v0_1[DIM-1] = {-3, 0.1, 0.1}, v0_2[DIM-1] = {0.1, 0.1, 0.1}, v0_3[DIM-1] = {3, 0.1, 0.1};                                 // initial velocity
     double h = 0.001;
 
     double time[N_STEPS], x_1[DIM][N_STEPS], x_2[DIM][N_STEPS], x_3[DIM][N_STEPS], v_1[DIM][N_STEPS], v_2[DIM][N_STEPS], v_3[DIM][N_STEPS], a_1[DIM][N_STEPS], a_2[DIM][N_STEPS], a_3[DIM][N_STEPS];
@@ -43,18 +43,23 @@ int main(){
     x_1[0][0] = x0_1[0];
     x_2[0][0] = x0_2[0];
     x_3[0][0] = x0_3[0];
+
     v_1[0][0] = v0_1[0];
     v_2[0][0] = v0_2[0];
     v_3[0][0] = v0_3[0];
+
     x_1[1][0] = x0_1[1];
-    x_3[1][0] = x0_2[1];
-    v_1[1][0] = x0_3[1];
-    v_2[1][0] = v0_1[1];
-    x_2[1][0] = v0_2[1];
+    x_2[1][0] = x0_2[1];
+    x_3[1][0] = x0_3[1];
+
+    v_1[1][0] = v0_1[1];
+    v_2[1][0] = v0_2[1];
     v_3[1][0] = v0_3[1];
+
     x_1[2][0] = x0_1[2];
     x_2[2][0] = x0_2[2];
     x_3[2][0] = x0_3[2];
+
     v_1[2][0] = v0_1[2];
     v_2[2][0] = v0_2[2];
     v_3[2][0] = v0_3[2];
@@ -66,8 +71,6 @@ int main(){
         }
     }
 */
-std::cout<<x_1[0][0]<<std::endl;
-
 
 
 //Function for the Euler method (contrasta con quella presente sull'header)
@@ -85,17 +88,27 @@ for(int j=0; j<DIM-1; j++){
     x_1[j][i + 1] = x_1[j][i] + v_1[j][i] * h;
     x_2[j][i + 1] = x_2[j][i] + v_2[j][i] * h;
     x_3[j][i + 1] = x_3[j][i] + v_3[j][i] * h;
-    if (j==0 and i==0){
-        std::cout<<x_1[j][j+1]<<"="<<x_1[j][i]<<" + "<< v_1[j][i] <<"*"<< h<<std::endl;
-    }
-    }
-    std::cout<<x_1[0][0]<<std::endl;
-}
-    //std::cout<<x_1[0][0]<<std::endl; // dovrebbe essere -10
+    if (j==1 and i==1){ // l'array inizia a sporcarsi per j=1 e i=1
 
-    //std::cout<<x_1[0][1]<<std::endl;
-    // diamo delle condizioni iniziali e stampiamo l'array x_1
-    // TODO
+        std::cout<<"I valori utilizzati per calcolare l'accelerazione valgono "<<x_2[j][i]<<", "<<x_3[j][i]<<", "<<x_1[j][i]<<std::endl;
+        std::cout<<"L'accelerazione utilizzata per calcolare la velocità vale "<<a_2[j][i-1]<<std::endl;
+        std::cout<<"La velocità utilizzata per calcolare la posizione vale "<<v_2[j][i]<<std::endl;
+        std::cout<<"La posizione vale "<<x_2[j][i+1]<<std::endl;
+        std::cout<<"\n";
+
+    }
+    }
+    
+}
+
+// Alla ricerca del bug perduto
+std::cout<<"Considero la posizione del corpo B nei primi step:\n"; //non coincidono con quelli su postions_B.csv!
+std::cout<<"x, "<<"y, "<<" z"<<std::endl;
+std::cout<<x_2[0][0]<<", "<<x_2[1][0]<<", "<<x_2[2][0]<<std::endl;
+std::cout<<x_2[0][1]<<", "<<x_2[1][1]<<", "<<x_2[2][1]<<std::endl;
+std::cout<<x_2[0][2]<<", "<<x_2[1][2]<<", "<<x_2[2][2]<<std::endl;
+
+
     std::ofstream output_file_A("positions_A.csv");
     std::ofstream output_file_B("positions_B.csv");
     std::ofstream output_file_C("positions_C.csv");
@@ -105,8 +118,8 @@ for(int j=0; j<DIM-1; j++){
     
     for(int i = 0; i<N_STEPS-1; i++){
         output_file_A << x_1[0][i] << ";" << x_1[1][i] << ";" << x_1[2][i]<< std::endl;
-        output_file_B << x_2[0][i] << ";" << x_1[1][i] << ";" << x_1[2][i]<< std::endl;
-        output_file_C << x_3[0][i] << ";" << x_1[1][i] << ";" << x_1[2][i]<< std::endl;
+        output_file_B << x_2[0][i] << ";" << x_2[1][i] << ";" << x_2[2][i]<< std::endl;
+        output_file_C << x_3[0][i] << ";" << x_3[1][i] << ";" << x_3[2][i]<< std::endl;
     }    
     output_file_A.close();
     output_file_B.close(); 
