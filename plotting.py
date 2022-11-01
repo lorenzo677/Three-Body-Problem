1f55ab90c1300c61dd423665294faf532257bdad
Lorenzo Barsotti <lorenzo.barsotti@hotmail.com>
2022-11-01 11:34:10 +0100

    Small changes

--- plotting.py
+++ plotting.py
@@ -15,10 +15,12 @@ fig = plt.figure()
  
 # syntax for 3-D projection
 ax = plt.axes(projection ='3d')
-
-ax.plot3D(file_positions_A.x, file_positions_A.y,file_positions_A.z)
-ax.plot3D(file_positions_B.x, file_positions_B.y,file_positions_B.z)
-ax.plot3D(file_positions_C.x, file_positions_C.y,file_positions_C.z)
+ax.plot3D(file_positions_A.x, [0]*len(file_positions_A),file_positions_A.z)
+ax.plot3D(file_positions_B.x, [0]*len(file_positions_A),file_positions_B.z)
+ax.plot3D(file_positions_C.x, [0]*len(file_positions_A),file_positions_C.z)
+# ax.plot3D(file_positions_A.x, file_positions_A.y,file_positions_A.z)
+# ax.plot3D(file_positions_B.x, file_positions_B.y,file_positions_B.z)
+# ax.plot3D(file_positions_C.x, file_positions_C.y,file_positions_C.z)
 ax.set_xlabel('x')
 ax.set_ylabel('y')
 ax.set_zlabel('z')
