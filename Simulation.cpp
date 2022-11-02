d9890e3cc4129113adfb26f02928f3593db55ddb
Lorenzo Barsotti <lorenzo.barsotti@hotmail.com>
2022-11-02 17:53:04 +0100

    Corrected two bugs

--- Simulation.cpp
+++ Simulation.cpp
@@ -7,7 +7,7 @@
 #define DIM 4
 #define G  10//6.67408e-11
 #define N_BODIES 3
-#define N_STEPS 1000
+#define N_STEPS 20000
 
 std::array<float, 3> compute_vector_cm(float mass_1, float mass_2, float mass_3, float *vector_1, float *vector_2, float *vector_3){   
     std::array<float, 3> vector_cm;
@@ -25,15 +25,15 @@ float distance(float pos_1[DIM], float pos_2[DIM]){
 
 float acceleration(float mass_1, float mass_2, float pos_1, float pos_2, float pos_3){ 
     //compute the acceleration along one axis of the body 3
-    return -1 * G * (mass_1 * (pos_3-pos_1) / pow(abs(pos_3-pos_1), 3) - G * mass_2 * (pos_3-pos_2) / pow(abs(pos_3-pos_2), 3));
+    return -1 * G * (mass_1 * (pos_3-pos_1) / pow(abs(pos_3-pos_1), 3) + mass_2 * (pos_3-pos_2) / pow(abs(pos_3-pos_2), 3));
 }
 
 int main(){
     // BE CAREFUL: If starting position of the body on one axis is the same, acceleration will be to inf.
     double mass_1 = 10, mass_2 = 20, mass_3 = 30;                       // masses
-    double x0_1[DIM-1] = {-10, 10, -11}, x0_2[DIM-1] = {0.5, 0.5, 0.5}, x0_3[DIM-1] = {10, 14, 12};                                  // initial positions
-    double v0_1[DIM-1] = {-3, 0.1, 0.1}, v0_2[DIM-1] = {0.1, 0.1, 0.1}, v0_3[DIM-1] = {3, 0.1, 0.1};                                 // initial velocity
-    double h = 0.001;
+    double x0_1[DIM-1] = {-10, 10, -11}, x0_2[DIM-1] = {0, 0, 0}, x0_3[DIM-1] = {10, 14, 12};                                  // initial positions
+    double v0_1[DIM-1] = {-3, 0, 0}, v0_2[DIM-1] = {0, 0, 0}, v0_3[DIM-1] = {3, 0, 0};                                 // initial velocity
+    double h = 0.0005;
 
     double time[N_STEPS], x_1[DIM][N_STEPS], x_2[DIM][N_STEPS], x_3[DIM][N_STEPS], v_1[DIM][N_STEPS], v_2[DIM][N_STEPS], v_3[DIM][N_STEPS], a_1[DIM][N_STEPS], a_2[DIM][N_STEPS], a_3[DIM][N_STEPS];
           
@@ -74,23 +74,23 @@ int main(){
 
 
 //Function for the Euler method (contrasta con quella presente sull'header)
-for(int j=0; j<DIM-1; j++){
-    for (int i=0; i<N_STEPS-1; i++){
-    
+
+for (int i=0; i<N_STEPS-1; i++){
+    for(int j=0; j<DIM-1; j++){
     a_1[j][i] = acceleration(mass_2, mass_3, x_2[j][i], x_3[j][i], x_1[j][i]);
     a_2[j][i] = acceleration(mass_1, mass_3, x_1[j][i], x_3[j][i], x_2[j][i]);
     a_3[j][i] = acceleration(mass_1, mass_2, x_1[j][i], x_2[j][i], x_3[j][i]);
-
+    
     v_1[j][i + 1] = v_1[j][i] + a_1[j][i] * h;
 	v_2[j][i + 1] = v_2[j][i] + a_2[j][i] * h;
-	v_2[j][i + 1] = v_3[j][i] + a_3[j][i] * h;
-
+	v_3[j][i + 1] = v_3[j][i] + a_3[j][i] * h;
+    
     x_1[j][i + 1] = x_1[j][i] + v_1[j][i] * h;
     x_2[j][i + 1] = x_2[j][i] + v_2[j][i] * h;
     x_3[j][i + 1] = x_3[j][i] + v_3[j][i] * h;
     
-    if (j==1 and i==1){ // l'array inizia a sporcarsi per j=1 e i=1
-
+    if (j==0 and i==1){ // l'array inizia a sporcarsi per j=1 e i=1
+        std::cout<<v_3[j][i-1]<<'+'<< a_3[j][i]<< '*'<< h;
         std::cout<<"I valori utilizzati per calcolare l'accelerazione valgono "<<x_2[j][i]<<", "<<x_3[j][i]<<", "<<x_1[j][i]<<std::endl;
         std::cout<<"L'accelerazione utilizzata per calcolare la velocità vale "<<a_3[j][i-1]<<std::endl;
         std::cout<<"La velocità utilizzata per calcolare la posizione vale "<<v_3[j][i]<<std::endl;
@@ -102,14 +102,16 @@ for(int j=0; j<DIM-1; j++){
     
 }
 
-// Alla ricerca del bug perduto
-std::cout<<"Considero l'accelerazione del corpo 3 nei primi step:\n"; //Il problema è a_3 sull'asse y.
-std::cout<<"x, "<<"y, "<<"z"<<std::endl;
-std::cout<<a_3[0][0]<<", "<<a_3[1][0]<<", "<<a_3[2][0]<<std::endl;
-std::cout<<a_3[0][1]<<", "<<a_3[1][1]<<", "<<a_3[2][1]<<std::endl;
-std::cout<<a_3[0][2]<<", "<<a_3[1][2]<<", "<<a_3[2][2]<<std::endl;
-
-
+    // Alla ricerca del bug perduto
+    std::cout<<"Considero la velocita del corpo 3 nei primi step:\n"; //Il problema è a_3 sull'asse y.
+    std::cout<<"x, "<<"y, "<<"z"<<std::endl;
+    std::cout<<v_3[0][0]<<", "<<v_3[1][0]<<", "<<v_3[2][0]<<std::endl;
+    std::cout<<v_3[0][1]<<", "<<v_3[1][1]<<", "<<v_3[2][1]<<std::endl;
+    std::cout<<v_3[0][2]<<", "<<v_3[1][2]<<", "<<v_3[2][2]<<std::endl;
+    std::cout<<v_3[0][3]<<", "<<v_3[1][3]<<", "<<v_3[2][3]<<std::endl;
+    std::cout<<v_3[0][4]<<", "<<v_3[1][4]<<", "<<v_3[2][4]<<std::endl;
+    std::cout<<v_3[0][5]<<", "<<v_3[1][5]<<", "<<v_3[2][5]<<std::endl;
+    std::cout<<v_3[0][25]<<", "<<v_3[1][25]<<", "<<v_3[2][25]<<std::endl;
 
     std::ofstream output_file_A("positions_A.csv");
     std::ofstream output_file_B("positions_B.csv");
