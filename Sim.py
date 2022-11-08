416571198199c837b7fc080425cd5053b802988a
Lorenzo Barsotti <lorenzo.barsotti@hotmail.com>
2022-11-08 18:26:31 +0100

    saving data on files

--- Sim.py
+++ Sim.py
@@ -70,17 +70,13 @@ def F(x, v, t, A, B, C, j ):
         return (-1 * G * (mass_A * (x-A.z) / pow(np.sqrt(pow(posx_C-A.x,2)+pow(posy_C-A.y,2)+pow(x-A.z,2)), 3) + mass_B * (x-posz_B) / pow(np.sqrt(pow(posx_C-posx_B,2)+pow(posy_C-posy_B,2)+pow(x-posz_B,2)), 3)))
         
 
-h = 0.001
+h = 0.11
 
 A = Planet(10, (-10, 10, -11), (-3, 0, 0))  
 B = Planet(10, (0, 0, 0), (0, 0, 0))
 C = Planet(10, (10, 14, 12), (0, 0, 0))
 
-time = np.arange(0,80000, N_STEPS)
-
-
-
-
+time = np.arange(0,N_STEPS*h, N_STEPS)
 
 mass_A = A.mass
 mass_B = B.mass
@@ -90,47 +86,43 @@ for  i in range(len(time)):
     time[i]= i * h
 
 
-    
-
 def x_derivatives(x, t, A,  B,  C ):
     return [x[1], (-1 * G * (A.mass * (x[0]-A.positionx) / pow(np.sqrt(pow(x[0]-A.positionx,2)+pow(C.positiony-A.positiony,2)+pow(C.positionz-A.positionz,2)), 3) + B.mass * (x[0]-B.positionx) / pow(np.sqrt(pow(x[0]-B.positionx,2)+pow(C.positiony-B.positiony,2)+pow(C.positionz-B.positionz,2)), 3))), x[1], (-1 * G * (A.mass * (x[1]-A.positiony) / pow(np.sqrt(pow(C.positionx-A.positionx,2)+pow(x[1]-A.positiony,2)+pow(C.positionz-A.positionz,2)), 3) + B.mass * (x[1]-B.positiony) / pow(np.sqrt(pow(C.positionx-B.positionx,2)+pow(x[1]-B.positiony,2)+pow(C.positionz-B.positionz,2)), 3))), x[1], (-1 * G * (A.mass * (x[2]-A.positionz) / pow(np.sqrt(pow(C.positionx-A.positionx,2)+pow(C.positiony-A.positiony,2)+pow(x[2]-A.positionz,2)), 3) + B.mass * (x[2]-B.positionz) / pow(np.sqrt(pow(C.positionx-B.positionx,2)+pow(C.positiony-B.positiony,2)+pow(x[2]-B.positionz,2)), 3)))]
-
-
-for i in range(80000):
-    positionsAx, velocityAx, positionsAy, velocityAy, positionsAz, velocityAz = odeint(x_derivatives, (A.positionx, A.velocityy, A.positiony, A.velocityy, A.positionz, A.velocityz),[0, 0.01], args=((C, B, A))).T
-
-    positionsBx, velocityBx, positionsBy, velocityBy, positionsBz, velocityBz = odeint(x_derivatives, (B.positionx, B.velocityy, B.positiony, B.velocityy, B.positionz, B.velocityz),[0, 0.01], args=((A, C, B))).T
-
-    positionsCx, velocityCx, positionsCy, velocityCy, positionsCz, velocityCz = odeint(x_derivatives, (C.positionx, C.velocityy, C.positiony, C.velocityy, C.positionz, C.velocityz),[0, 0.01], args=((A, B, C))).T
-
-
-    A.positionx = positionsAx
-    A.positiony = positionsAy
-    A.positionz = positionsAz
-
-    B.positionx = positionsBx
-    B.positiony = positionsBy
-    B.positionz = positionsBz
-
-    C.positionx = positionsCx
-    C.positiony = positionsCy
-    C.positionz = positionsCz
-
-print(positionsAx, positionsAy, positionsAz)
 
 output_file_A=open("positions_A.csv", "w")
 output_file_B=open("positions_B.csv", "w")
 output_file_C=open("positions_C.csv", "w")
 
-output_file_A.write("x;y;z;t")
-output_file_B.write("x;y;z;t")
-output_file_C.write("x;y;z;t")
-    
-# for i in range(N_STEPS):
-#     output_file_A.write( f"{x_A[0][i]};{x_A[1][i]}; {x_A[2][i]};{time[i]}")
-#     output_file_A.write( f"{x_B[0][i]};{x_B[1][i]}; {x_B[2][i]};{time[i]}")
-#     output_file_A.write( f"{x_C[0][i]};{x_C[1][i]}; {x_C[2][i]};{time[i]}")
-  
+output_file_A.write("x;y;z;t\n")
+output_file_B.write("x;y;z;t\n")
+output_file_C.write("x;y;z;t\n")
+output_file_A.write( f"{A.positionx}; {A.positiony}; {A.positionz}; {0}\n")
+output_file_B.write( f"{B.positionx}; {B.positiony}; {B.positionz}; {0}\n")
+output_file_C.write( f"{C.positionx}; {C.positiony}; {C.positionz}; {0}\n")
+
+for i in range(N_STEPS):
+    positionsAx, velocityAx, positionsAy, velocityAy, positionsAz, velocityAz = odeint(x_derivatives, (A.positionx, A.velocityy, A.positiony, A.velocityy, A.positionz, A.velocityz),[0, h*i], args=((C, B, A))).T
+
+    positionsBx, velocityBx, positionsBy, velocityBy, positionsBz, velocityBz = odeint(x_derivatives, (B.positionx, B.velocityy, B.positiony, B.velocityy, B.positionz, B.velocityz),[0, h*i], args=((A, C, B))).T
+
+    positionsCx, velocityCx, positionsCy, velocityCy, positionsCz, velocityCz = odeint(x_derivatives, (C.positionx, C.velocityy, C.positiony, C.velocityy, C.positionz, C.velocityz),[0, h*i], args=((A, B, C))).T
+
+    A.positionx = positionsAx[1]
+    A.positiony = positionsAy[1]
+    A.positionz = positionsAz[1]
+
+    B.positionx = positionsBx[1]
+    B.positiony = positionsBy[1]
+    B.positionz = positionsBz[1]
+
+    C.positionx = positionsCx[1]
+    C.positiony = positionsCy[1]
+    C.positionz = positionsCz[1]
+    output_file_A.write( f"{A.positionx}; {A.positiony}; {A.positionz}; {h*i}\n")
+    output_file_B.write( f"{B.positionx}; {B.positiony}; {B.positionz}; {h*i}\n")
+    output_file_C.write( f"{C.positionx}; {C.positiony}; {C.positionz}; {h*i}\n")
+
+
 output_file_A.close()
 output_file_B.close() 
 output_file_C.close()
