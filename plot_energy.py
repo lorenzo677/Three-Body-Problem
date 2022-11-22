f257e6bdc8164fb0b3e6e9ea6b8958975e14fede
Lorenzo Barsotti <lorenzo.barsotti@hotmail.com>
2022-11-22 18:15:09 +0100

    small changes

--- plot_energy.py
+++ plot_energy.py
@@ -1,15 +1,16 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
-dati_euler = pd.read_csv("Total_energy_euler.csv")
-dati_rk4 = pd.read_csv("Total_energy_rk4.csv")
-dati_verlet = pd.read_csv("Total_energy_verlet.csv")
+dati_euler = pd.read_csv("Total_energy_euler.csv",sep=';')
+dati_rk4 = pd.read_csv("Total_energy_rk4.csv",sep=';')
+dati_verlet = pd.read_csv("Total_energy_verlet.csv",sep=';')
 
-plt.plot(dati_euler.energy, label='euler')
-plt.plot(dati_verlet.energy, label='verlet')
-plt.plot(dati_rk4.energy, label='rk4')
+plt.plot(dati_euler.k+dati_euler.p, label='euler')
+plt.plot(dati_verlet.k+dati_verlet.p, label='verlet')
+plt.plot(dati_rk4.k+dati_rk4.p, label='rk4')
 
 plt.legend()
 plt.ylabel("total energy")
+
 plt.xlabel("Step")
 plt.show()
