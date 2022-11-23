51d3cb41de79327159f5e2d6bcbc462fc1953343
Lorenzo Barsotti <lorenzo.barsotti@hotmail.com>
2022-11-23 09:56:36 +0100

    time in x axis

--- plot_energy.py
+++ plot_energy.py
@@ -5,9 +5,9 @@ dati_euler = pd.read_csv("Total_energy_euler.csv",sep=';')
 dati_rk4 = pd.read_csv("Total_energy_rk4.csv",sep=';')
 dati_verlet = pd.read_csv("Total_energy_verlet.csv",sep=';')
 
-plt.plot(dati_euler.k+dati_euler.p, label='euler')
-plt.plot(dati_verlet.k+dati_verlet.p, label='verlet')
-plt.plot(dati_rk4.k+dati_rk4.p, label='rk4')
+#plt.plot(dati_euler.k+dati_euler.p, label='euler')
+# plt.plot(dati_verlet.k+dati_verlet.p, label='verlet')
+plt.plot([i*0.008 for i in range(len(dati_rk4.k))] ,dati_rk4.k+dati_rk4.p, label='rk4')
 
 plt.legend()
 plt.ylabel("total energy")
