50034acbb8cfacfa8476539448ac6a0efb9c07be
Lorenzo Barsotti <lorenzo.barsotti@hotmail.com>
2022-11-24 23:09:20 +0100

    now we are using the time scale on x axis

--- plot_energy.py
+++ plot_energy.py
@@ -3,10 +3,10 @@ import pandas as pd
 
 dati_euler = pd.read_csv("Total_energy_euler.csv",sep=';')
 dati_rk4 = pd.read_csv("Total_energy_rk4.csv",sep=';')
-dati_verlet = pd.read_csv("Total_energy_verlet.csv",sep=';')
+dati_leapfrog = pd.read_csv("Total_energy_leapfrog.csv",sep=';')
 
-#plt.plot(dati_euler.k+dati_euler.p, label='euler')
-# plt.plot(dati_verlet.k+dati_verlet.p, label='verlet')
+plt.plot([i*0.002 for i in range(len(dati_leapfrog.k))], dati_euler.k+dati_euler.p, label='euler')
+plt.plot([i*0.002 for i in range(len(dati_leapfrog.k))], dati_leapfrog.k+dati_leapfrog.p, label='leapfrog')
 plt.plot([i*0.008 for i in range(len(dati_rk4.k))] ,dati_rk4.k+dati_rk4.p, label='rk4')
 
 plt.legend()
