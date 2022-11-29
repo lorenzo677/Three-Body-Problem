5d7e2c8edaecbf793fb5f158599842acd35cae40
Keivan Amini <keivan.amini@outlook.it>
2022-11-29 19:19:36 +0100

    tried to fix spring formulas.

--- Simulation.cpp
+++ Simulation.cpp
@@ -30,13 +30,13 @@ void Initialize(){
 }
 
 static constexpr int DIM = 4;
-static constexpr double G = 10;
+static constexpr double G = 0;
 static constexpr int N_BODIES = 3;
 static constexpr int N_STEPS = 70000;
 
 // Spring
 static constexpr int K_CONST = 5000;
-static constexpr int L0 = 10; 
+static constexpr double L0 = 11.18; 
 // float l0 = sqrt(pow(L0X,2)+pow(L0Y,2)+pow(L0Z,2)); // modulo lunghezza a riposo molla
 
 // std::array <double, 3> L0 = {L0X, L0Y, L0Z};
@@ -105,19 +105,25 @@ std::array<double, 3> AngularMomentum(std::array<double, 3> cm, Planet planet){
     return L;
 }
 
+
+// Commenti - robe da sistemare:
+// 1) Il pianeta B ha molta più accelerazione elastica del pianeta C inspiegabilmente
+// 2) Bisogna introdurre il respingimento della molla che fino ad adesso non si verifica
+
+
 double springC(double x, double v, double t, Planet A, Planet B, Planet C, int axe){
     // Compute the gravitational + spring acceleration of the body C, specifying the axis.
-    return (-1 * (G * (A.m * (C.x[axe]-A.x[axe]) / pow(distance(A.x, C.x), 3) + B.m * (C.x[axe]-B.x[axe]) / pow(distance(B.x, C.x), 3)))); // + (K_CONST / C.m) * (B.x[axe]-C.x[axe]) * (distance(B.x, C.x)-L0) / distance(B.x, C.x));
+    return (-1 * (G * (A.m * (C.x[axe]-A.x[axe]) / pow(distance(A.x, C.x), 3) + B.m * (C.x[axe]-B.x[axe]) / pow(distance(B.x, C.x), 3))) + (K_CONST / C.m) * (C.x[axe]-B.x[axe]-L0) * (C.x[axe]-B.x[axe]) / (pow(distance(B.x, C.x), 3)));
 }
 
 double springB(double x, double v, double t, Planet A, Planet B, Planet C, int axe){
     // Compute the gravitational + spring acceleration of the body C, specifying the axis.
-    return (-1 * (G * (A.m * (C.x[axe]-A.x[axe]) / pow(distance(A.x, C.x), 3) - B.m * (C.x[axe]-B.x[axe]) / pow(distance(B.x, C.x), 3)))); //+ (K_CONST / B.m) * (C.x[axe]-A.x[axe])  * (distance(A.x, C.x) - L0) / distance(A.x, C.x));
+    return (-1 * (G * (A.m * (C.x[axe]-A.x[axe]) / pow(distance(A.x, C.x), 3) + B.m * (C.x[axe]-B.x[axe]) / pow(distance(B.x, C.x), 3))) + (K_CONST / C.m) * (C.x[axe]-A.x[axe]-L0) * (C.x[axe]-A.x[axe]) / (pow(distance(A.x, C.x), 3)));
 }
 
 double acceleration(double x, double v, double t, Planet A, Planet B, Planet C, int axe){
     // Compute the gravitational acceleration of the body C, specifying the axis.
-    return (-1 * G * (A.m * (C.x[axe]-A.x[axe]) / pow(distance(A.x, C.x), 3) - B.m * (C.x[axe]-B.x[axe]) / pow(distance(B.x, C.x), 3)));
+    return (-1 * G * (A.m * (C.x[axe]-A.x[axe]) / pow(distance(A.x, C.x), 3) + B.m * (C.x[axe]-B.x[axe]) / pow(distance(B.x, C.x), 3)));
 }
 
 int main(int argc, char** argv){
@@ -125,15 +131,15 @@ int main(int argc, char** argv){
     double h = 0.002;
 
 
-    // Planet A(0, -10, 0, 0, 0, 0, 0);   // corpi allineati sull'asse delle x
-    // Planet B(10, 10, 0, 0, -2, 3, 0);
-    // Planet C(10, 10, 5, 0, 2, -3, 0);
+     Planet A(5000, 0, 0, 0, 0, 0, 0);   // corpi allineati sull'asse delle x
+     Planet B(10, -5, 0, 0, 0, 0, 0);
+     Planet C(10, 5, 0, 0, 0, 0, 0);
 
     // CONFIGURAZIONI BELLE
 
-    Planet A(0, -10, 0, 0, 0, -5, 0); 
-    Planet B(100, 0, 0, 0, 0, 5, 0);
-    Planet C(100, 13, 14, 12, 0, -5, 0);
+    //Planet A(0, -10, 0, 0, 0, -5, 0); 
+    //Planet B(100, 0, 0, 0, 0, 5, 0);
+    //Planet C(100, 13, 14, 12, 0, -5, 0);
 
     double x_A[DIM][3];
     double x_B[DIM][3];
@@ -378,7 +384,7 @@ int main(int argc, char** argv){
 
             break;
             default:
-                std::cout<<"Insert an argument between: euler, rk or leapfrog"<<std::endl;
+                std::cout<<"Insert an argument between: euler, rk4 or leapfrog"<<std::endl;
                 return 0;
         }
     }else{
